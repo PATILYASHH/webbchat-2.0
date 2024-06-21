f6efe3dbb953cd2290c174850f4b0adef81b6eb6
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-06-21 09:48:24 +0000

    update

--- js/client.js
+++ js/client.js
@@ -1,8 +1,9 @@
-const socket = io('https://localhost:3000');
-
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector('.container');
-
 const name = prompt("Enter your name to join");
-  socket.emit('new-user-joined', window.name);
+socket.emit("new-user-joined".name);
+
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
+const messageContainer = document.querySelector(".container");
+
