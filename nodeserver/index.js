5cc43dc6770224b4c146b1fe8bc542c25875fd23
Yash patil <149749700+PATILYASHH@users.noreply.github.com>
2024-06-22 16:58:13 +0000

    update

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,14 +1,23 @@
 const io = require('socket.io')(8000);
-const user = {};
 
-io.on('connection', socket =>{
+const users = {}; // Use 'users' for clarity
+
+io.on('connection', socket => {
+  // Handle new user joining
   socket.on('new-user-joined', name => {
-    console.log("new-user", name);
-    user[socket.id] = name;
-    socket.broadcast.emit('user-joined', name);
+    console.log("New user joined:", name);
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name); // Broadcast the new user joined event
   });
 
+  // Handle sending messages
   socket.on('send', message => {
-    socket.broadcast.emit('receive', { message: message, name: user[socket.id] });
-  })
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  });
+
+  // Handle user disconnection
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-left', users[socket.id]);
+    delete users[socket.id];
+  });
 });
